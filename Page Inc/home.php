85245cfe902e54ae351e8ccc4489112df8559a66
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-03 09:48:54 +0000

    Initial Commit

---
+++ Page Inc/home.php
@@ -0,0 +1,7 @@
+<?php
+	require("page.inc");
+	$homepage = new Page();
+	$homepage->content ="<p>Welcome to the home of TTA Consulting. Please take some time to get to know us.</p>
+		<p>We specialize in serving your business needsand hope to hear from you soon.</p>";
+	$homepage->Display();
+?>
